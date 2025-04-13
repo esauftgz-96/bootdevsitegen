0341b6304fadbc8f8337b6e5c4b2ba59983c0b90
esauftgz-96 <benjaminljq@gmail.com>
2025-04-13 19:23:22 +0800

    build.sh

---
+++ base.sh
@@ -0,0 +1,1 @@
+python3 src/main.py "https://github.com/esauftgz-96/bootdevsitegen"
--- src/main.py
+++ src/main.py
@@ -3,10 +3,14 @@ from htmlnode import *
 from mkd_blocks import *
 import shutil
 import os
+import sys
 
 def main():
+    basepath = "/"
+    if len(sys.agrv) > 1:
+        basepath = sys.agrv[1]
     static_to_public()
-    generate_pages_recursive("content","template.html","public")
+    generate_pages_recursive("content","template.html","public",basepath)
 
 def copy_recursive(src_path, dst_path):
     for item in os.listdir(src_path):
@@ -32,23 +36,23 @@ def static_to_public():
     # Start recursive copy
     copy_recursive("static", "public")
 
-def generate_page(from_path, template_path, dest_path):
-    print (f"Generating page from {from_path} to {dest_path} using {template_path}.")
-    with open(from_path, 'r') as file:
-        markdown = file.read()
-    with open(template_path, 'r') as file:
-        template = file.read()
-    processed_markdown = markdown_to_html_node (markdown).to_html()
-    title = extract_title(markdown)
-    template = template.replace("{{ Title }}",title)
-    template = template.replace("{{ Content }}",processed_markdown)
-    dir_path = os.path.dirname(dest_path)
-    if not os.path.exists(dir_path):
-        os.makedirs(dir_path)
-    with open(dest_path, 'w') as file:
-        file.write(template)
+# def generate_page(from_path, template_path, dest_path):
+#     print (f"Generating page from {from_path} to {dest_path} using {template_path}.")
+#     with open(from_path, 'r') as file:
+#         markdown = file.read()
+#     with open(template_path, 'r') as file:
+#         template = file.read()
+#     processed_markdown = markdown_to_html_node (markdown).to_html()
+#     title = extract_title(markdown)
+#     template = template.replace("{{ Title }}",title)
+#     template = template.replace("{{ Content }}",processed_markdown)
+#     dir_path = os.path.dirname(dest_path)
+#     if not os.path.exists(dir_path):
+#         os.makedirs(dir_path)
+#     with open(dest_path, 'w') as file:
+#         file.write(template)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path,basepath):
     for item in os.listdir(dir_path_content):
         src_item = os.path.join(dir_path_content, item)
         dst_item = os.path.join(dest_dir_path, item)
@@ -62,6 +66,8 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
             title = extract_title(markdown)
             template = template.replace("{{ Title }}",title)
             template = template.replace("{{ Content }}",processed_markdown)
+            template = template.replace('href="/',f'href="{basepath}')
+            template = template.replace('src="/',f'src="{basepath}')
             dir_path = os.path.dirname(dst_item)
             if not os.path.exists(dir_path):
                 os.makedirs(dir_path)
